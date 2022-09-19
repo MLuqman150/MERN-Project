fd7de5df0cc526218f65265623c8ca429f664fec
MLuqman150 <muhammadluqman12080@outlook.com>
2022-09-19 08:24:52 +0500

    Updated the Product Screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 // import data from "../data";
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from "axios";
 import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -40,6 +41,9 @@ function HomeScreen() {
         fetchData();
     }, []);
     return (<div>
+        <Helmet>
+            <title>Amazona</title>
+        </Helmet>
         <h1>Featured Products </h1>
         <div className="products">
             {
