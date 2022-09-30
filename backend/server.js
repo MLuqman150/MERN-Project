89752ac66838f5c09cd5a46d6005f2b0fcf61bdf
MLuqman150 <muhammadluqman12080@outlook.com>
2022-09-30 12:26:11 +0500

    Imporved the cart functionality and made a new api

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,17 @@ app.get('/api/products/slug/:slug', (req, res) => {
     }
 });
 
+// For checking the quantity of certain product in the inventory
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.status(404).send({ message: 'Product not found' });
+    }
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
